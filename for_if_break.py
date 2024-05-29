4cfa5c8f8ed090e3f180aa2ea4926221389126a0
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-05-29 10:25:34 +0530

    Add files via upload

---
+++ for_if_break.py
@@ -0,0 +1,6 @@
+# for loop using break statement
+Animals = ["Tiger","Lion","Elephant"]           # storage data from Animals variable
+for x in Animals:                               # suing for loop executing Animals line by line single data execution
+    print(x)
+    if x=="Lion":                               # break the execution for Lion
+        break
